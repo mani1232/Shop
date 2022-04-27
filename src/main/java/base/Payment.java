915647c9909fcb35b9acce446b3947101d967d40
mani123 <worldmandiareload@gmail.com>
2022-04-27 18:33:57 +0200

    Add Payment

---
+++ src/main/java/base/Payment.java
@@ -0,0 +1,9 @@
+package base;
+
+// Интерфейс платежа
+public interface Payment {
+
+    double calculatePayment(int quota, double price);
+
+    double calculatePayment(double payment, double discount);
+}
