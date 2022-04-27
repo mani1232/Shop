e033804ee412ea885a8a770d9e1c20be6bef0332
mani123 <worldmandiareload@gmail.com>
2022-04-27 18:32:46 +0200

    Update Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import controllers.PurchaseController;
+import models.Purchase;
+import views.PurchaseView;
+
+// Входная точка в программу/приложение
 public class Main {
+
     public static void main(String[] args) {
 
+        Purchase model = new Purchase();
+        PurchaseView view = new PurchaseView(model);
+        PurchaseController controller = new PurchaseController(model, view);
+        // Запуск программы/приложения
+        controller.runApp();
     }
 }
