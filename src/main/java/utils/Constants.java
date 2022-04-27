288f40b3003cbb99f280bf0fb06b030e98dd70b2
mani123 <worldmandiareload@gmail.com>
2022-04-27 18:33:27 +0200

    Add Constants

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Constants {
+
+    public static final String EMPTY_NAME_MSG = "Empty value! Enter name: ";
+    public static final String NAN_QUANTITY_MSG = "\"%s\" - not a number!%nEnter quantity: ";
+    public static final String INCORRECT_QUANTITY_MSG = "Incorrect value! Enter quantity: ";
+    public static final String NAN_PRICE_MSG = "\"%s\" - not a number!%nEnter price: ";
+    public static final String INCORRECT_PRICE_MSG = "Incorrect value! Enter price: ";
+}
